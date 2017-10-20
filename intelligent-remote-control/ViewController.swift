1f13e1732caacdde0379eed90714309244681137
Shun <qi.shun.wnag@icloud.com>
2017-10-20 13:58:48 +0800

    Initial Commit

---
+++ intelligent-remote-control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intelligent-remote-control
+//
+//  Created by QiShunWang on 2017/10/20.
+//  Copyright © 2017年 ising99. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
